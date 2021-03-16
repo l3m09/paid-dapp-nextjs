ca4149363a1c67c093efca6a0018059300665904
Alvaro Vivas <alvijacu@gmail.com>
2021-03-16 10:54:57 -0600

    Integrate connect selector modal [skip ci]

---
+++ data/connectSelector.ts
@@ -0,0 +1,20 @@
+const connectSelector: any[] = [
+  {
+    code: 'partyANotLogged',
+    name: 'Party A Not Logged',
+  },
+  {
+    code: 'partyALogged',
+    name: 'Party A Logged',
+  },
+  {
+    code: 'partyBNotLogged',
+    name: 'Party B Not Logged',
+  },
+  {
+    code: 'partyBLogged',
+    name: 'Party B Logged',
+  },
+];
+
+export default connectSelector;
