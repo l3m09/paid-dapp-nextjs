9af673e13cabb226cb1c89caa7596fbb22520158
Sigfredo <sigfredo.jalvarado@gmail.com>
2021-05-23 23:19:29 -0400

    Fix on main menu with toggle buttons

--- pages/binance_chain.tsx
+++ pages/binance_chain.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
-import { useDispatch, useSelector } from 'react-redux';
-import setOpenMenu from '../redux/actions/menu';
 
 const BinanceChain: React.FC = () => {
 
-  const dispatch = useDispatch();
-  const isOpen = useSelector((state: any) => state.menuReducer.isOpen);
 
   return (
     <>
@@ -17,20 +13,7 @@ const BinanceChain: React.FC = () => {
       <div className="agreements m-0 p-0 px-4 container-fluid">
         <div className="row m-0 p-0 h-100">
           <div className="col-12 py-4 d-flex">
-            
-            <span
-              tabIndex={0}
-              className="d-flex"
-              role="button"
-              onClick={() => dispatch(setOpenMenu(!isOpen))}
-              onKeyDown={() => dispatch(setOpenMenu(!isOpen))}
-            >
-              <img
-                className="d-inline mr-5 hambunguer"
-                src="/assets/icon/hamburguer.svg"
-                alt=""
-              />
-            </span>
+
             <h3 className="d-flex mr-auto">Coming soon</h3>
           </div>
         </div>
--- pages/smart_agreements.tsx
+++ pages/smart_agreements.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
-import { useDispatch, useSelector } from 'react-redux';
-import setOpenMenu from '../redux/actions/menu';
 
 const SmartAgreements: React.FC = () => {
-
-  const dispatch = useDispatch();
-  const isOpen = useSelector((state: any) => state.menuReducer.isOpen);
 
   return (
     <>
@@ -17,20 +12,7 @@ const SmartAgreements: React.FC = () => {
       <div className="agreements m-0 p-0 px-4 container-fluid">
         <div className="row m-0 p-0 h-100">
           <div className="col-12 py-4 d-flex">
-            
-            <span
-              tabIndex={0}
-              className="d-flex"
-              role="button"
-              onClick={() => dispatch(setOpenMenu(!isOpen))}
-              onKeyDown={() => dispatch(setOpenMenu(!isOpen))}
-            >
-              <img
-                className="d-inline mr-5 hambunguer"
-                src="/assets/icon/hamburguer.svg"
-                alt=""
-              />
-            </span>
+
             <h3 className="d-flex mr-auto">Coming soon</h3>
           </div>
         </div>
